34448c0b949b04c2fc8f4b31689d39d89bf34709
andanomi <andanomi@alumni.uv.es>
2026-04-21 18:27:59 +0200

    Implementamos la funcion de densidad teorica completa; cambiando los parametros por a y b

--- R/triang_dist.R
+++ R/triang_dist.R
@@ -1,7 +1,17 @@
-if (any(min > max, na.rm =T)) {
+dtriang <- function(x,a,b,mode) {
+if (any(a > b, na.rm =T)) {
   stop("Error: 'min' no puede ser mayor que 'max'")
 }
 
-if (any(mode < min | mode > max, na.rm = T)) {
+if (any(mode < a | mode > b, na.rm = T)) {
   stop("Error: 'mode' no puede estar fuera de '[min,max]'")
+}
+
+resultado <- ifelse(x < a | x > b) {resultado = 0}
+            ifelse(a <= x & x <=c) {
+              resultado = (2*(x-a)) / ((b-a)*(c-a))
+            }else{
+             resultado = (2*(b-x)) / ((b-a)*(b-c))
+            }
+  return(resultado)
 }
