b37c580816678fea5974e74c78ce8491189d95b2
andanomi <andanomi@alumni.uv.es>
2026-04-28 18:20:58 +0200

    corregimos errores para el lintr

--- R/triang_dist.R
+++ R/triang_dist.R
@@ -1,6 +1,6 @@
 #' Triangular Distribution Density
 #'
-#' Computes the probability density function (PDF) of the triangular distribution.
+#' Computes the probability density function (PDF) of triangular distribution.
 #'
 #' @param x A numeric vector of quantiles.
 #' @param min The lower limit of the distribution.
@@ -8,24 +8,20 @@
 #' @param mode The mode (peak) of the distribution.
 #' @return A numeric vector containing the density values
 #' @export
+dtriang <- function(x, min, max, mode) {
+  if (any(min > max, na.rm = TRUE)) {
+    stop("Error: 'min' cannot be greater than 'max'")
+  }
 
-dtriang <- function(x,min,max,mode) {
-if (any(min > max, na.rm =T)) {
-  stop("Error: 'min' no puede ser mayor que 'max'")
-}
-
-if (any(mode < min | mode > max, na.rm = T)) {
-  stop("Error: 'mode' no puede estar fuera de '[min,max]'")
-}
-
-resultado <- ifelse(x < min | x > max,0,
-                    ifelse(min <= x & x <=mode,
-                           (2*(x-min)) / ((max-min)*(mode-min)),
-                                        (2*(max-x)) / ((max-min)*(max-mode))
-                          )
-                    )
-
-  return(resultado)
+  if (any(mode < min | mode > max, na.rm = TRUE)) {
+    stop("Error: 'mode' cannot be out of '[min,max]'")
+  }
+  ifelse(x < min | x > max, 0,
+    ifelse(min <= x & x <= mode,
+      (2 * (x - min)) / ((max - min) * (mode - min)),
+      (2 * (max - x)) / ((max - min) * (max - mode))
+    )
+  )
 }
 
 #' Cumulative Distribution
@@ -38,25 +34,27 @@ resultado <- ifelse(x < min | x > max,0,
 #' @param mode The mode (peak) of the distribution.
 #' @return A numeric vector containing the cumulative probability.
 #' @export
-
-ptriang <- function(q,min,max,mode) {
-  if (any(min > max, na.rm =T)) {
-    stop("Error: 'min' no puede ser mayor que 'max'")
+ptriang <- function(q, min, max, mode) {
+  if (any(min > max, na.rm = TRUE)) {
+    stop("Error: 'min' cannot be greater than 'max'")
   }
 
-  if (any(mode < min | mode > max, na.rm = T)) {
-    stop("Error: 'mode' no puede estar fuera de '[min,max]'")
+  if (any(mode < min | mode > max, na.rm = TRUE)) {
+    stop("Error: 'mode' cannot be out of '[min,max]'")
   }
 
-  res <- ifelse(q <= min,0,
-                ifelse(q >= max, 1,
-                       ifelse(q <= mode,
-                              ((q-min)^2) / ((max-min)*(mode-min)),
-                              1 - ((max-q)^2) / ((max-min)*(max-mode))
-                       )
-                )
+  ifelse(
+    q <= min, 0,
+    ifelse(
+      q >= max, 1,
+      ifelse(
+        q <= mode,
+        ((q - min)^2) / ((max - min) * (mode - min)),
+        1 - ((max - q)^2) / ((max - min) * (max - mode))
+      )
+    )
   )
-  return(res)
+
 }
 
 #' Triangular Distribution Quantiles
@@ -69,22 +67,25 @@ ptriang <- function(q,min,max,mode) {
 #' @param mode The mode (peak) of the distribution.
 #' @return A numeric vector containing the corresponding quantiles.
 #' @export
-
-qtriang <- function(p,min,max,mode) {
-  if (any(min > max, na.rm =T)) {
-    stop("Error: 'min' no puede ser mayor que 'max'")
+qtriang <- function(p, min, max, mode) {
+  if (any(min > max, na.rm = TRUE)) {
+    stop("Error: 'min' cannot be greater than 'max'")
   }
 
-  if (any(mode < min | mode > max, na.rm = T)) {
-    stop("Error: 'mode' no puede estar fuera de '[min,max]'")
+  if (any(mode < min | mode > max, na.rm = TRUE)) {
+    stop("Error: 'mode' cannot be out of '[min,max]'")
   }
-  if (any(p < 0 | p > 1,na.rm = T)){
-    stop("Error: 'p' debe estar estrictamente en el intervalo [0,1] ")
+
+  if (any(p < 0 | p > 1, na.rm = TRUE)) {
+    stop("Error: 'p' must be in [0,1]")
   }
-  res <- ifelse(p <= (mode-min) / (max-min),
-                sqrt(p *(max-min)*(mode-min)) + min,
-                max - sqrt((1-p)*(max-min)*(max-mode)))
-  return(res)
+
+  ifelse(
+    p <= (mode - min) / (max - min),
+    sqrt(p * (max - min) * (mode - min)) + min,
+    max - sqrt((1 - p) * (max - min) * (max - mode))
+  )
+
 }
 
 #' Triangular Distribution Random Generation
@@ -96,18 +97,17 @@ qtriang <- function(p,min,max,mode) {
 #' @param max The upper limit of the distribution.
 #' @param mode The mode (peak) of the distribution.
 #' @return A numeric vector of random generated values.
+#' @importFrom stats runif
 #' @export
-
-rtriang <- function(n,min,max,mode){
-  if (any(min > max, na.rm =T)) {
-    stop("Error: 'min' no puede ser mayor que 'max'")
+rtriang <- function(n, min, max, mode) {
+  if (any(min > max, na.rm = TRUE)) {
+    stop("Error: 'min' cannot be greater than 'max'")
   }
 
-  if (any(mode < min | mode > max, na.rm = T)) {
-    stop("Error: 'mode' no puede estar fuera de '[min,max]'")
+  if (any(mode < min | mode > max, na.rm = TRUE)) {
+    stop("Error: 'mode' cannot be out of '[min,max]'")
   }
 
   u <- runif(n)
-  res <- qtriang(u,min,max,mode)
-  return(res)
+  qtriang(u, min, max, mode)
 }
